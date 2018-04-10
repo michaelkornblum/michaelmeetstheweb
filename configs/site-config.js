52c165d5d9f3a64cbbf76fedc1a0e34d232ac68d
michaelkornblum <mkornblum71@gmail.com>
2018-04-10 13:08:05 -0500

    add site metadata

--- configs/site-config.js
+++ configs/site-config.js
@@ -1,1 +1,8 @@
-// this is a test
+module.exports = {
+  siteName: 'Michael Meets the Web',
+  siteUrl: 'michaelmeetstheweb.com',
+  description: 'the blog site of Michael Kornblum',
+  author: 'Michael Kornblum',
+  generatorName: 'Metalsmith',
+  generatorUrl: 'http://metalsmith.io',
+};
