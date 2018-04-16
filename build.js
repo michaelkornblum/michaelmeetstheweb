d18f91ce301b3293f7ac2044bdd52adae183a759
michaelkornblum <mkornblum71@gmail.com>
2018-04-16 05:58:07 -0500

    add responsive image handling

--- build.js
+++ build.js
@@ -4,23 +4,52 @@ const layouts = require('metalsmith-layouts');
 const permalinks = require('metalsmith-permalinks');
 const collections = require('metalsmith-collections');
 const config = require('./configs/site-config');
+const jquery = require('metalsmith-jquery');
 
 metalsmith(__dirname)
   .metadata(config)
+  .use(markdown({
+    gfm: true,
+  }))
+  .use(jquery(($) => {
+    $('p img').parent().addClass('killMe');
+    $('p img').insertBefore('p');
+    $('.killMe').remove();
+
+    $('img').each((i, el) => {
+      const image = $(el);
+      const source = image.attr('src');
+      const delimeter = source.lastIndexOf('.');
+      const fileName = source.slice(0, delimeter);
+      const fileExt = source.slice(delimeter);
+      const srcset = `
+        ${fileName}-320w${fileExt} 320w,
+        ${fileName}-480w${fileExt} 480w,
+        ${fileName}-800w${fileExt} 320w, 
+      `;
+      const sizes = `
+        (max-width: 320px) 280px,
+        (max-width: 480px) 440px,
+        800px
+      `;
+      const src = `${fileName}-320w${fileExt}`;
+      image.attr('data-srcset', srcset);
+      image.attr('data-sizes', sizes);
+      image.attr('data-src', src);
+    });
+  }
+))
   .use(collections({
     posts: {
-      pattern: 'posts/**/*.md',
+      pattern: 'posts/**/*.html',
       sortBy: 'date',
       reverse: true,
     },
   }))
   .use(collections({
     pages: {
-      pattern: 'pages/**/*.md',
+      pattern: 'pages/**/*.html',
     },
-  }))
-  .use(markdown({
-    gfm: true,
   }))
   .use(permalinks())
   .use(layouts({
--- src/pages/gallery.md
+++ src/pages/gallery.md
@@ -3,4 +3,7 @@ title: gallery
 layout: article.pug
 ---
 
-![Alt text](/path/to/img.jpg)
+## Gallery
+![Image 1](/images/image-1-placeholder.jpg)
+![Image 2](/images/image-2-placeholder.jpg)
+![Image 3](/images/image-3-placeholder.jpg)
